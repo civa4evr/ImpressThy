ed0c3c25a3e6f7f04d979a546c56c35fd02620cc
civa4evr <64471579+civa4evr@users.noreply.github.com>
2020-04-29 15:43:29 +0530

    Add files via upload

--- AddExpense.js
+++ AddExpense.js
@@ -6,18 +6,29 @@ class Exp extends React.Component{
     this.state={
       newExpense: 0,
       expDesc: '',
+      totExpense: 0,
     };
   }
-  acceptIncome = (event) => {
-    this.setstate(
-      {[event.target.name]:event.target.value}
+  acceptExpense = (event) => {
+    let varType = event.target.name;
+    let varValue = event.target.value;
+    this.setState(
+      {[varType]:varValue}
     );
+    }
+
+  submitExpense = (event) => {
+    this.setState(
+      {totExpense: this.state.totExpense + this.state.newExpense}
+    );
+    console.log(this.state.totExpense);
   }
   render() {
     return (
     <form>
-     <input type="number" name="newExpense" placeholder="Add the Expense here"></input>
-     <input type="text" name="expDesc" placeholder="Add comment as needed"></input>
+    <h1>{this.state.newExpense}</h1>
+     <input type="number" name="newExpense" placeholder="Add the Expense here" onChange={this.acceptExpense}></input>
+     <input type="text" name="expDesc" placeholder="Add comment as needed" onChange={this.acceptExpense}></input>
      <input type="submit" name="submit" value="Submit"></input>
      <input type="reset" name="reset" value="Reset"></input>
     </form>
